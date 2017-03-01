c6f132736218392d1e7c71303677d9e5416b86fd
Nils Barlaug <nils.barlaug@gmail.com>
2017-03-01 12:37:56 +0100

    Add PersonStream

---
+++ intellivator/PersonStream.py
@@ -0,0 +1,31 @@
+from intellivator.elevator_environment import NewPersonEvent, NoEvent
+
+
+class PersonStream():
+    def __init__(self, person_file):
+        self.person_file = person_file
+        self.next_event = NoEvent
+        self._read_next_event()
+
+    def _read_next_event(self):
+        line = self.person_file.readline()
+        if line:
+            time, arrival_floor, destination_floor = line.split('\t')
+            time = float(time)
+            arrival_floor = int(arrival_floor) - 1
+            destination_floor = int(destination_floor) - 1
+            self.next_event = NewPersonEvent(
+                time,
+                arrival_floor,
+                destination_floor
+            )
+        else:
+            self.next_event = NoEvent
+
+    def get_next(self):
+        event = self.next_event
+        self._read_next_event()
+        return event
+
+    def peek(self):
+        return self.next_event
---
+++ intellivator/test_PersonStream.py
@@ -0,0 +1,62 @@
+import unittest
+from intellivator.PersonStream import PersonStream
+from intellivator.elevator_environment import NewPersonEvent, NoEvent
+from io import StringIO
+
+class PersonStreamTest(unittest.TestCase):
+
+    def setUp(self):
+        lines = [
+            '0\t1\t3',
+            '123.4\t1\t2',
+            '234.5\t4\t2'
+        ]
+        self.events = [
+            NewPersonEvent(
+                time = 0,
+                arrival_floor = 0,
+                destination_floor = 2
+            ),
+            NewPersonEvent(
+                time = 123.4,
+                arrival_floor = 0,
+                destination_floor = 1
+            ),
+            NewPersonEvent(
+                time = 234.5,
+                arrival_floor = 3,
+                destination_floor = 1
+            )
+        ]
+        self.person_file = StringIO('\n'.join(lines))
+
+    def test_peek_first(self):
+        person_stream = PersonStream(self.person_file)
+        self.assertEqual(person_stream.peek(), self.events[0])
+        self.assertEqual(person_stream.peek(), self.events[0])
+        self.assertEqual(person_stream.get_next(), self.events[0])
+        self.assertEqual(person_stream.peek(), self.events[1])
+        self.assertEqual(person_stream.peek(), self.events[1])
+        self.assertEqual(person_stream.get_next(), self.events[1])
+
+    def test_get_first(self):
+        person_stream = PersonStream(self.person_file)
+        self.assertEqual(person_stream.get_next(), self.events[0])
+        self.assertEqual(person_stream.peek(), self.events[1])
+        self.assertEqual(person_stream.peek(), self.events[1])
+        self.assertEqual(person_stream.get_next(), self.events[1])
+        self.assertEqual(person_stream.peek(), self.events[2])
+        self.assertEqual(person_stream.peek(), self.events[2])
+
+    def test_empty(self):
+        person_stream = PersonStream(self.person_file)
+        for event in self.events:
+            self.assertEqual(person_stream.peek(), event)
+            self.assertEqual(person_stream.peek(), event)
+            self.assertEqual(person_stream.get_next(), event)
+
+        self.assertEqual(person_stream.peek(), NoEvent)
+        self.assertEqual(person_stream.peek(), NoEvent)
+        self.assertEqual(person_stream.get_next(), NoEvent)
+        self.assertEqual(person_stream.get_next(), NoEvent)
+        self.assertEqual(person_stream.peek(), NoEvent)
