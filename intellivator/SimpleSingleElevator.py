855ead6c6273f2836593458817c5d3b96c2a2508
Nils Barlaug <nils.barlaug@gmail.com>
2017-03-22 11:18:35 +0100

    Fix elevator schedule bug

--- intellivator/SimpleSingleElevator.py
+++ intellivator/SimpleSingleElevator.py
@@ -9,8 +9,11 @@ class SimpleSingleElevator():
     def init(self, env_state):
         self.elevator = list(env_state.elevator_states.keys())[0]
 
-    def _should_be_loaded(self, elevator_position, waiting_persons):
-        return elevator_position == waiting_persons.arrival_floor
+    def _should_be_loaded(self, elevator_position, waiting_person):
+        return (
+            elevator_position == waiting_person.arrival_floor
+            and (waiting_person.person.destination_floor - elevator_position) * self.mode > 0
+        )
 
     def _should_be_picked_up(self, elevator_position, waiting_person):
             delta_pos = waiting_person.arrival_floor - elevator_position
@@ -56,17 +59,23 @@ class SimpleSingleElevator():
         free_capacity -= len(persons_to_load)
         persons_to_pick_up = persons_to_pick_up[:free_capacity]
 
-        if not persons_to_load and not persons_to_pick_up and not persons:
+
+        plan = [p.destination_floor for p in elevator_state.persons]
+        plan.extend([wp.arrival_floor for wp in persons_to_pick_up])
+        plan.extend([wp.person.destination_floor for wp in persons_to_load])
+        plan.sort(key=lambda f: self.mode * f)
+
+        if not plan:
             if env_state.waiting_persons:
                 self.mode = Direction.UP if self.mode == Direction.DOWN else Direction.DOWN
                 return self.get_next_actions(env_state, event)
             else:
                 return []
 
-        plan = [p.destination_floor for p in elevator_state.persons]
-        plan.extend([wp.arrival_floor for wp in persons_to_pick_up])
-        plan.extend([wp.person.destination_floor for wp in persons_to_load])
-        plan.sort(key=lambda f: self.mode * f)
+        if (plan[0] - position) * self.mode < 0:
+            print(self.mode, plan[0], position)
+            self.mode = Direction.UP if self.mode == Direction.DOWN else Direction.DOWN
+            return self.get_next_actions(env_state, event)
 
         if plan[0] == position:
             return []
